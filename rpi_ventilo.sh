7b44dd0ea491ad93d5f1300b15826df9cd520dfa
EpixFr <epix@konectik.fr>
2018-03-22 21:46:23 +0100

    ajout commentaires

--- rpi_ventilo.sh
+++ rpi_ventilo.sh
@@ -3,28 +3,37 @@
 #Param seuil de décenchement du ventilateur
 seuil_temperature=41 
 
+#Recuperation de la temperature du proc
 temperature=`vcgencmd measure_temperature`
 
+#On retient que la partie entière de l'information
 temperature=${temperature:5:2}
 
+#fichier de valeur de la broche gpio
 fichier="/sys/class/gpio/gpio25/value"
 
+#Test de l'existence du fichier
 if [ ! -f /sys/class/gpio/gpio25/value ]
 then
   gpio_value=0
 else
   gpio_value=1
 fi
-echo $gpio_value
 
+
+#Comparaison de la température avec le seuil paramétré
 if [ $temperature -lt $seuil_temperature ]
 then  
+   # si la température est faible on arrête le ventilateur
+   # si il est déjà demarré
    if [ $gpio_value == 1 ]
    then
      echo 0 > /sys/class/gpio/gpio25/value
      echo 25 > /sys/class/gpio/unexport
    fi
 else
+  # si la température est trop haute on démarre le ventilateur
+  # si il n'est pas déjà démarré
   if [ $gpio_value == 0 ]
    then
     echo 25 > /sys/class/gpio/export
